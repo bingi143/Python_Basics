8c95225b5a496a030b7b5158d9f6c3c11986a7a9
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-09 13:41:00 +0530

    Displaying last and first items

---
+++ ML_Basic_Programs/3_Last_first_list.py
@@ -0,0 +1,9 @@
+'''@Author: Venkatesh
+@Date: 2024-08-05 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2021-02-11 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+"" '''
+color_list=["Red","Green","White","Black"]
+print(color_list[0],color_list[-1])
