3529dbb969c3b70c7f853624208fe41302fa73c0
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-09 13:35:10 +0530

    Last name first name reverse

---
+++ ML_Basic_Programs/Last_First_Name_reverse.py
@@ -0,0 +1,12 @@
+'''@Author: Venkatesh
+@Date: 2024-08-02 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2021-02-11 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+"" '''
+First_name = str(input("Enter Your first name:"))
+Last_name = str(input("Enter Last name:"))
+print(First_name[::-1],end=(' '))
+print(Last_name[::-1])
+
