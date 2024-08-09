6157e90ac51dd11ff3eb7afa09b6977b64ee90d7
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-09 13:43:10 +0530

    Print Calender of month

---
+++ ML_Basic_Programs/5_Calender_Month.py
@@ -0,0 +1,13 @@
+'''@Author: Venkatesh
+@Date: 2024-08-05 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2021-02-11 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+"" '''
+import calendar
+
+year = int(input("Enter year last two digits:"))
+month = int(input("Enter month: "))
+
+print(calendar.month(year,month))
