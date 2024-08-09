fbc472f1c936837a7b3538d56f26ff890b8d2c74
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-09 13:47:44 +0530

    Concadinate all list elements into string

---
+++ ML_Basic_Programs/9_Concatenate_list_String.py
@@ -0,0 +1,15 @@
+'''@Author: Venkatesh
+@Date: 2024-08-05 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2021-02-11 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+"" '''
+def concronate(list):
+    st=str()
+    for i in list:
+        st+=(str(i))
+    return st
+list=[1,2,34,5,'hello']
+print(concronate(list))
+
