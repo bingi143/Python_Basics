27c0b11bcc92964a97f93f8e3d52dd26cd7a0003
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-09 13:44:47 +0530

    Specific value in group or not

---
+++ ML_Basic_Programs/7_Check_in_List.py
@@ -0,0 +1,11 @@
+'''@Author: Venkatesh
+@Date: 2024-08-05 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2021-02-11 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+"" ''' 
+list=[1,5,8,3]
+
+print(3 in list)
+print(-1 in list)
