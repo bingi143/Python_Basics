d1fcf40c4be511b7eda0451649ea9416a46a35c0
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-09 13:41:59 +0530

    Functions abs()

---
+++ ML_Basic_Programs/4_Function.py
@@ -0,0 +1,11 @@
+'''@Author: Venkatesh
+@Date: 2024-08-05 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2021-02-11 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+"" '''
+def print_Function_doc(function):
+    help(function)
+
+print(print_Function_doc(abs))
