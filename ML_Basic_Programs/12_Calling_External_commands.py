219b3a5519379c446516e810eb2d3029cad1fc30
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-09 13:50:22 +0530

    Calling Exteranl commands

---
+++ ML_Basic_Programs/12_Calling_External_commands.py
@@ -0,0 +1,9 @@
+'''@Author: Venkatesh
+@Date: 2024-08-05 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2021-02-11 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+"" '''
+import subprocess
+subprocess.call['ls','-l']
