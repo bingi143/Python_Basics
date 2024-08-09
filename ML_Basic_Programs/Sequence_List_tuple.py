649bdd92f5117eb41786ba1a34b42e617704cc3d
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-09 13:36:00 +0530

    Sequence of items in list

---
+++ ML_Basic_Programs/Sequence_List_tuple.py
@@ -0,0 +1,13 @@
+'''@Author: Venkatesh
+@Date: 2024-08-05 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2021-02-11 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+"" '''
+in_put=input("Enter sequence of numbers using :")
+
+list_=in_put.split(",")
+_tuple=tuple(list_)
+print(list_)
+print(_tuple)
